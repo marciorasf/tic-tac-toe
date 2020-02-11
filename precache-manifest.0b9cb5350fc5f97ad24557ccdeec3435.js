d5a5e264493605ffc337f20a0859ad4d92cd8dab
Marcio Roberto Andrade Souza Filho <marciorasf@gmail.com>
2020-02-11 17:03:58 -0300

    Updates

---
+++ precache-manifest.0b9cb5350fc5f97ad24557ccdeec3435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6a5ba6b58c8ae5078e6993ac664d40",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a4032de6e4ae02c84115",
+    "url": "/tic-tac-toe/static/css/main.74246ace.chunk.css"
+  },
+  {
+    "revision": "3aceaec3d70a0f94bdaa",
+    "url": "/tic-tac-toe/static/js/2.00ece94a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.00ece94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4032de6e4ae02c84115",
+    "url": "/tic-tac-toe/static/js/main.0ae890f0.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  },
+  {
+    "revision": "a07b5b8d2859afa17081308f51d083bc",
+    "url": "/tic-tac-toe/static/media/left-arrow.a07b5b8d.svg"
+  }
+]);
