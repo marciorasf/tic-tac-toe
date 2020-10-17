e325eabbc61c760705f5ae0985d6f680ed8fba63
marciorasf <marciorasf@gmail.com>
2020-10-17 14:05:24 -0300

    add hard bot

--- src/gameLogic/hardBot.js
+++ src/gameLogic/hardBot.js
@@ -1,1 +1,12 @@
-export default function hardBotNextSquare() {}
+import impossibleBotNextSquare from "./impossibleBot";
+import mediumBotNextSquare from "./mediumBot";
+
+export default function hardBotNextSquare(squares, botPlayer) {
+  const impossibleBotRatio = 0.8;
+
+  if (Math.random() < impossibleBotRatio) {
+    impossibleBotNextSquare(squares, botPlayer);
+  } else {
+    mediumBotNextSquare(squares, botPlayer);
+  }
+}
